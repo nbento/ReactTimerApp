9e4de00b373f47939b26d3cde38d95d1fc32ae93
nbento <ratinhodaprogramacao@gmail.com>
2017-01-11 13:20:43 +0000

    Teste React Timer app

---
+++ app/app.jsx
@@ -0,0 +1,38 @@
+var React 		= require('react');
+var ReactDOM 	= require('react-dom');
+
+//.......... ES5
+/*	var Route 		= require('react-router').Route;
+	var Router  	= require('react-router').Router;
+	var IndexRoute 	= require('react-router').IndexRoute;
+	var hashHistory = require('react-router').hashHistory;
+*/	
+//.......... ES6
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+//.......... 
+var Main 	= require('Main');   
+
+
+//Load foundation (CARREGA UM FILE CSS DIRECTA/ NUM FILE JSX(!!!))  (Lec.51 ~10:00)
+//css! »»» 'by default the require doesn't know how to load a css file'; FORMA QUE PERMITE AO REQUIRE LOADAR UM CSS  
+//style! »»» FORMA QUE injectar o css file no HTML
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
+//alias em webpack.config.js
+require('style!css!sass!applicationStyles');
+//<Route path="countdown" component={Countdown} />
+//<IndexRoute path="timer" component={Timer} />
+
+
+ReactDOM.render( 
+			
+			<Router history={hashHistory}>
+				<Route path="/" component={Main}>
+					
+		  		</Route>
+			</Router>,
+
+  			document.getElementById('app')
+);
---
+++ app/components/Main.jsx
@@ -0,0 +1,56 @@
+var React = require('react');
+var Navigation = require('Navigation');
+
+/*
+var Main = React.createClass(
+{
+	render: function()
+	{
+		return (<div>
+					<h2>3 Title Main Component!!!</h2>
+					 <Nav />
+					{this.props.children}
+				</div>)
+	}
+});
+*/
+
+var Main = (props) => {
+	return (
+			<div>
+ 				<div>
+					<div>
+						<Navigation/>
+						<p>Main.jsx Rendered</p>
+						{props.children}
+					</div>
+				</div>
+			</div>
+		);
+}
+
+/*
+var Main = React.createClass(
+{
+	mainHandleSearch: function(location)
+	{
+		alert('mainHandleSearch   location' + location);
+	},
+	render: function()
+	{
+		//<Nav navSearch={this.mainHandleSearch} />
+		return (<div>
+							<div className="row">
+									<div>
+
+									</div>
+							</div>
+							<Nav navSearch={this.mainHandleSearch} />
+							<h2>Main Component!!!</h2>
+							{this.props.children}
+						</div>
+			)
+	}
+});
+*/
+module.exports = Main;
